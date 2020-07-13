9510e6ef4a07d10c291287296f37b09439dd9dce
Andrew McDougal <andrew@ontrendwebsites.com>
2020-07-13 22:42:51 +1000

    WIP setup css and script

--- functions.php
+++ functions.php
@@ -24,3 +24,12 @@ function sf_child_theme_dequeue_style() {
  */
 
 
+function ontrend_scripts_and_styles() {
+  wp_register_script('ontrend_script', get_stylesheet_directory_uri() . '/library/js/ontrend.js', array(), '1.0', true);
+  wp_enqueue_script('ontrend_script');
+
+  wp_register_style('ontrend_style', get_stylesheet_directory_uri() . '/library/css/ontrend.css', array(), '1.0', false);
+  wp_enqueue_style('ontrend_style');
+}
+  
+add_action( 'wp_enqueue_scripts', 'ontrend_scripts_and_styles' );  
